a795df34d814906e96f6dcbe66135a163e37da08
EriseldaHoxha <eriseldahoxha100@gmail.com>
2023-07-09 11:03:46 +0200

    fix

--- src/globals/config/routes.js
+++ src/globals/config/routes.js
@@ -1,18 +1,32 @@
-import React from "react";
-import Authenticate from "../../pages/Authenticate/Authenticate";
-import Home from "../../pages/Home/Home";
+import React from 'react'
+import Authenticate from '../../pages/Authenticate/Authenticate'
+import Home from '../../pages/Home/Home'
+import Document from '../../pages/Document/Document'
+import SearchPage from '../../pages/Search.js/SearchPage'
 
 export const routes = [
   {
-    name: "Authenticate",
-    path: "/",
+    name: 'Authenticate',
+    path: '/',
     private: false,
     element: <Authenticate />,
   },
   {
-    name: "Home",
-    path: "/home",
-    private: true,
+    name: 'Home',
+    path: '/home',
+    private: false,
     element: <Home />,
   },
-];
+  {
+    name: 'Document',
+    path: 'home/document',
+    private: false,
+    element: <Document />,
+  },
+  {
+    name: 'Search Page',
+    path: '/search',
+    private: false,
+    element: <SearchPage />,
+  },
+]
