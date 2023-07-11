f320f64661be15fd70f5d511dabaaf9e0babdb76
Leonard <ldurmishi@airfind.com>
2023-07-11 18:06:16 +0200

    changes

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -1,24 +1,24 @@
-import { ContainerOutlined, ReadOutlined } from '@ant-design/icons'
-export const BASE_API = 'http://38.242.237.75:3333'
-//export const BASE_API = "http://localhost:3002";
+import { ContainerOutlined, ReadOutlined } from "@ant-design/icons";
+// export const BASE_API = 'http://38.242.237.75:3333'
+export const BASE_API = "http://localhost:3002";
 //http://38.242.237.75:3333
 
 export const controlLabels = [
   {
-    label: 'Kontrolli I',
+    label: "Kontrolli I",
     key: 0,
     icon: <ContainerOutlined />,
   },
   {
-    label: 'Kontrolli II',
+    label: "Kontrolli II",
     key: 1,
     icon: <ContainerOutlined />,
     disabled: false,
   },
   {
-    label: 'Kontrolli III',
+    label: "Kontrolli III",
     key: 2,
     icon: <ContainerOutlined />,
     disabled: false,
   },
-]
+];
